5181ea1c1cbda2c226dd9f690b190f7d59707fa5
sbondarenko <sbondarenko@smart-consulting.ru>
2020-07-14 00:44:45 +0700

    Commit to delete over closing curly brace.

--- test-junit/src/test/java/SampleTest.java
+++ test-junit/src/test/java/SampleTest.java
@@ -7,13 +7,20 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class SampleTest {
 
     protected  static WebDriver driver;
 
-   private Logger logger = LogManager.getLogger(SampleTest.class);
+    private Logger logger = LogManager.getLogger(SampleTest.class);
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Запуск драйвера");
+    }
+
+
 
 
        @Test
@@ -24,16 +31,27 @@ public class SampleTest {
 
     }
 
-    @Before
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("Драйвер поднят");
-    }
+/*
+        public void testingCookies(){
+            driver.manage().addCookie(new Cookie("Otus 1",  "Value 1"));
+            driver.manage().addCookie(new Cookie("Otus 2", "Value 2"));
+            Cookie cookie = new Cookie ("Otus 3",  "Value 3");
+            driver.manage().addCookie(cookie);
+            driver.manage().addCookie(new Cookie("Otus 4", "Value 4"));
+            driver.manage().getCookies();
+            logger.info( "Cookies cooked");
+            driver.manage().getCookieNamed("Otus 1");
+            driver.manage().deleteCookie(cookie);
+            driver.manage().deleteAllCookies();
+            driver.manage().getCookies();
+        }
+*/
+
     @Test
     public void openPage() {
         driver.get("https://otus.ru/");
-        logger.info("Открыта страница отус");
+        logger.info("Главная OTUS");
+//        List<WebElement> menu driver.findELements(By.)
     }
 
     @After
